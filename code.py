a0230079b9d900a625a2402ef6e394553c9c8046
Nature-24 <obinna.iloduba24@yahoo.com>
2023-03-15 01:22:17 -0400

    python learning code

---
+++ code.py
@@ -0,0 +1,210 @@
+#!/bin/bash
+
+# print("hello world")
+
+# name = input("please enter your name:")
+# print("Hello")
+# age = input("what is your age?")
+# print("you are qualified within the age bracket")
+
+
+# first_name = "obinna"
+# last_name = "iloduba"
+# print(first_name + last_name)
+# print("hello " + first_name + ' ' + last_name)
+
+# sentence = "the dog name is piggy"
+# print(sentence.upper())
+# print(sentence.lower())
+# print(sentence.capitalize())
+# print(sentence.count("a"))
+
+
+# first_name = "obinna"
+# last_name = "iloduba"
+# print(first_name.upper() + '' + last_name.lower())
+
+
+# name = input("what is your name?")
+# print("Hello welcome to the team")
+# print(name)
+
+
+# first_name = "obinna"
+# last_name = "iloduba"
+# print("Hello " + first_name + ' ' + last_name)
+
+# first_name = input("what is your first name")
+# last_name = input("what is your last name")
+# print("Hello " + first_name.capitalize() + ' ' + last_name.capitalize())
+
+
+# first_name = ("obinna")
+# last_name = ("iloduba")
+# output = f'Hello, {first_name} {last_name}'
+# print(output)
+
+# name = ("obinna")
+# age = 44
+# print(f"my name is {name} and my age is {age} years old")
+
+
+# first_name = ("obinna")
+# last_name = ("iloduba")
+# output = (f"Hello, {first_name} {last_name} ")
+# print(output)
+
+# pi = 3.14159
+# print(pi)
+
+
+# first_num = 6
+# second_num = 2
+# print(first_num ** second_num)
+
+
+# days_in_feb = 28
+# print(str(days_in_feb) + "days in February")
+
+# current_date = datetime.now()
+# print("today is: " + str(current_date))
+
+# school_name = input("what is the name of your school?")
+# degree = input("what degree do you have?")
+# output = (f"my school name is {school_name} and my degree is {degree}")
+# print(output)
+
+# print("Day: " + str(current_date.day))
+# print("Month: " + str(current_date.month))
+# I HAVE TO READ ON DATETIME TO UNDERSTAND IT VERY WELL
+
+# country = ('CANADA')
+# if country == "CANADA":
+#     print("oh you look like a canadian")
+# else:
+#     print("you are not from canada")
+
+
+# weight = float(input("what is the weight of your bag?"))
+# if weight == weight > 50.25:
+#     print('the weight is excess')
+# else:
+#     print("the weight is within the limit")
+
+
+# phone = input("what type of phone do you want?")
+# if phone == "samsung":
+#     print("Welcome to samsung")
+# else:
+#     print("We can order it for you with free shipping")
+
+
+# coffee_price = 1.50
+# number_of_coffees = 4
+# result = (coffee_price * number_of_coffees)
+# print(result)
+
+# quilt_width = 8
+# quilt_length = 12
+# print(quilt_width * quilt_length)
+
+# quilt_length = 8
+# print(quilt_width * quilt_length)
+
+
+# lovely_loveseat_description = ("Lovely Loveseat" ' ')
+# lovely_loveseat_description += ("Tufted polyester blend on wood" ' ')
+# lovely_loveseat_description += ("32 inches high  x 40 inches wide" ' ')
+# lovely_loveseat_description += ("Red or white")
+# print(lovely_loveseat_description)
+
+# lovely_loveseat_description = ("Lovely Loveseat" ' ')
+# lovely_loveseat_description += ("Tufted polyester blend on wood" ' ')
+# lovely_loveseat_description += (
+#     "32 inches high x 40 inches wide x 3 inches deep" ' ')
+# lovely_loveseat_description += ("Red or white")
+# print(lovely_loveseat_description)
+
+# # creating price for loveseat
+# lovely_loveseat_price = 254.00
+# print(lovely_loveseat_price)
+
+# # extending our inventory
+# stylish_settee_description = ("Stylish settee" ' ')
+# stylish_settee_description += ("Faux leather on birch" ' ')
+# stylish_settee_description += (
+#     "29.50 inches high x 54.75 inches wide x 28 inches deep" ' ')
+# stylish_settee_description += ("Black")
+# print(stylish_settee_description)
+
+# # Setting price for Stylish Settee
+# stylish_settee_price = 180.50
+# print(stylish_settee_price)
+
+# # creating Luxurious Lamp
+# luxurious_lamp_description = ("Luxurious Lamp" ' ')
+# luxurious_lamp_description += ("Glass and iron" ' ')
+# luxurious_lamp_description += ("36 inches tall" ' ')
+# luxurious_lamp_description += ("Brown with cream shade")
+# print(luxurious_lamp_description)
+
+# # Setting price for Luxurious Lamp
+# luxurious_lamp_description = 52.15
+# print(luxurious_lamp_description)
+
+# # Calculating sales tax
+# sales_tax = .088
+# print(sales_tax)
+
+# # First Customer Purchase
+# customer_one_total = 0
+# print(customer_one_total)
+
+# # Customer Purchase List
+# customer_one_itemization = ("")
+# print(customer_one_itemization)
+
+# #  Customer bought Lovely Loveseat
+# customer_one_total += lovely_loveseat_price
+# print(customer_one_total)
+
+# # Keeping track of Customer Purchase
+# customer_one_itemization += ("lovely_loveseat_price")
+# print(customer_one_itemization)
+
+# # Customer Purchase Luxurious Lamp
+# customer_one_total += luxurious_lamp_description
+# print(customer_one_total)
+
+# # Adding itemization to Luxurious Lamp
+# customer_one_total += luxurious_lamp_description
+# print(luxurious_lamp_description)
+
+# # Calculating Sales Tax
+# customer_one_tax = customer_one_total * sales_tax
+# print(customer_one_tax)
+
+# # Add Sales tax to Customer's total cost
+# customer_one_total += customer_one_tax
+# print(customer_one_total)
+
+# # Start Printing out receipt
+# print("Customer One Items")
+
+# # Print Customer itemization
+# print("customer_one_itemization")
+
+# Heading for total cost
+# print("Customer One Total")
+
+#   USING INPUT FUNCTION FOR USER
+# favorite_fruit = input("what is your favorite fruit?.")
+# print(f"oh cool, i like {favorite_fruit} but am cool with mango. ")
+
+# WROTE A SMALL CODE FOR VOTING
+# Country = input("what is your Country?")
+# Vote = input("Have you voted?")
+# print(
+#     f"Welcome to {Country} and since you said {Vote}\n
+#  thank you for your time")
+
